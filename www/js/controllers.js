d5a9ff6d22e153d12ebdb3f385a3cce35a042bbf
Jail81 <jail81@gmail.com>
2015-04-14 20:36:31 +0200

    Added modal.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,7 +3,7 @@ angular.module('ionicDemoTabs.controllers', [])
     .controller('CssCtrl', function ($scope) {
     })
 
-    .controller('JsComponentsCtrl', function ($scope, $ionicActionSheet, $ionicLoading) {
+    .controller('JsComponentsCtrl', function ($scope, $ionicActionSheet, $ionicLoading, $ionicModal) {
         $scope.showActionSheet = function () {
             $ionicActionSheet.show({
                 buttons: [
@@ -28,6 +28,17 @@ angular.module('ionicDemoTabs.controllers', [])
                 duration: 2000
             });
         };
+
+        $ionicModal.fromTemplateUrl('templates/js-samples/modal.html', {
+            scope: $scope,
+            animation: 'slide-in-up'
+        }).then(function(modal) {
+            $scope.modal = modal;
+        });
+        $scope.openModal = function() {
+            $scope.modal.show();
+        };
+
 
     })
 
