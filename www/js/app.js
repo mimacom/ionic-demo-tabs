c0d1cad38000ed314f4ec95e49efeec105258efe
Jail81 <jail81@gmail.com>
2015-04-14 17:38:34 +0200

    Added buttons.

--- www/js/app.js
+++ www/js/app.js
@@ -7,77 +7,77 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('ionicDemoTabs', ['ionic', 'starter.controllers', 'starter.services'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleLightContent();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: "/tab",
-    abstract: true,
-    templateUrl: "templates/tabs.html"
-  })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.css', {
-    url: '/css',
-    views: {
-      'tab-css': {
-        templateUrl: 'templates/tab-css.html',
-        controller: 'CssCtrl'
-      }
-    }
-  })
-
-  .state('tab.js', {
-      url: '/js',
-      views: {
-        'tab-js': {
-          templateUrl: 'templates/tab-js.html',
-          controller: 'JsComponentsCtrl'
-        }
-      }
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleLightContent();
+            }
+        });
     })
-    /*.state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-js': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })*/
 
-  .state('tab.event', {
-    url: '/event',
-    views: {
-      'tab-event': {
-        templateUrl: 'templates/tab-event.html',
-        controller: 'EventCtrl'
-      }
-    }
-  });
+    .config(function ($stateProvider, $urlRouterProvider) {
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/css');
+        // Ionic uses AngularUI Router which uses the concept of states
+        // Learn more here: https://github.com/angular-ui/ui-router
+        // Set up the various states which the app can be in.
+        // Each state's controller can be found in controllers.js
+        $stateProvider
 
-});
+            // setup an abstract state for the tabs directive
+            .state('tab', {
+                url: "/tab",
+                abstract: true,
+                templateUrl: "templates/tabs.html"
+            })
+
+            // Each tab has its own nav history stack:
+
+            .state('tab.css', {
+                url: '/css',
+                views: {
+                    'tab-css': {
+                        templateUrl: 'templates/tab-css.html',
+                        controller: 'CssCtrl'
+                    }
+                }
+            })
+
+            .state('tab.buttons', {
+                url: '/buttons',
+                views: {
+                    'tab-css': {
+                        templateUrl: 'templates/css/buttons.html'
+                    }
+                }
+            })
+
+            .state('tab.js', {
+                url: '/js',
+                views: {
+                    'tab-js': {
+                        templateUrl: 'templates/tab-js.html',
+                        controller: 'JsComponentsCtrl'
+                    }
+                }
+            })
+
+            .state('tab.event', {
+                url: '/event',
+                views: {
+                    'tab-event': {
+                        templateUrl: 'templates/tab-event.html',
+                        controller: 'EventCtrl'
+                    }
+                }
+            });
+
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/tab/css');
+
+    });
