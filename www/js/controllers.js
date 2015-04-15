d5c40ca2d01676add358bf2b82d5d4483bf3c003
Jail81 <jail81@gmail.com>
2015-04-15 15:24:52 +0200

    Added plugin

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -24,7 +24,7 @@ angular.module('ionicDemoTabs.controllers', [])
             });
         };
 
-        $scope.showLoading = function() {
+        $scope.showLoading = function () {
             $ionicLoading.show({
                 template: '<ion-spinner></ion-spinner>',
                 duration: 2000
@@ -34,14 +34,14 @@ angular.module('ionicDemoTabs.controllers', [])
         $ionicModal.fromTemplateUrl('templates/js-samples/modal.html', {
             scope: $scope,
             animation: 'slide-in-up'
-        }).then(function(modal) {
+        }).then(function (modal) {
             $scope.modal = modal;
         });
-        $scope.openModal = function() {
+        $scope.openModal = function () {
             $scope.modal.show();
         };
 
-        $scope.showAlert = function() {
+        $scope.showAlert = function () {
             var alertPopup = $ionicPopup.alert({
                 title: 'Warning',
                 template: 'This action is dangerous!!!'
@@ -51,13 +51,13 @@ angular.module('ionicDemoTabs.controllers', [])
             });
         };
 
-        $scope.showConfirm = function() {
+        $scope.showConfirm = function () {
             var confirmPopup = $ionicPopup.confirm({
                 title: 'Please confirm',
                 template: 'Are you sure you want to continue?'
             });
-            confirmPopup.then(function(res) {
-                if(res) {
+            confirmPopup.then(function (res) {
+                if (res) {
                     console.log('You are sure');
                 } else {
                     console.log('You are not sure');
@@ -65,7 +65,7 @@ angular.module('ionicDemoTabs.controllers', [])
             });
         };
 
-        $scope.showPopup = function() {
+        $scope.showPopup = function () {
             $scope.data = {};
 
             // An elaborate, custom popup
@@ -75,11 +75,11 @@ angular.module('ionicDemoTabs.controllers', [])
                 subTitle: 'Please use normal things',
                 scope: $scope,
                 buttons: [
-                    { text: 'Cancel' },
+                    {text: 'Cancel'},
                     {
                         text: '<b>Save</b>',
                         type: 'button-positive',
-                        onTap: function(e) {
+                        onTap: function (e) {
                             if (!$scope.data.wifi) {
                                 //don't allow the user to close unless he enters wifi password
                                 e.preventDefault();
@@ -91,67 +91,69 @@ angular.module('ionicDemoTabs.controllers', [])
                     }
                 ]
             });
-            myPopup.then(function(res) {
+            myPopup.then(function (res) {
                 console.log('Tapped!', res);
             });
-            $timeout(function() {
+            $timeout(function () {
                 myPopup.close(); //close the popup after 3 seconds for some reason
             }, 3000);
         };
 
         $ionicPopover.fromTemplateUrl('templates/js-samples/popover.html', {
             scope: $scope
-        }).then(function(popover) {
+        }).then(function (popover) {
             $scope.popover = popover;
         });
 
-        $scope.showPopover = function($event) {
+        $scope.showPopover = function ($event) {
             $scope.popover.show($event);
         };
-        $scope.closePopover = function() {
+        $scope.closePopover = function () {
             $scope.popover.hide();
         };
 
     })
 
-    .controller('EventCtrl', function ($scope, $cordovaDevice, $ionicPlatform, $ionicPopup) {
+    .controller('EventCtrl', function ($scope, $cordovaDevice, $cordovaNetwork, $ionicPlatform, $ionicPopup) {
         $ionicPlatform.ready(function () {
-            try{
+            try {
                 $scope.device = $cordovaDevice.getDevice();
+                $scope.network = $cordovaNetwork.getNetwork();
+                $scope.isOnline = $cordovaNetwork.isOnline();
             } catch (error) {
                 console.error(error);
             }
         });
 
-        $scope.$on('$ionicView.loaded', function() {
+        $scope.$on('$ionicView.loaded', function () {
             $scope.isLoaded = true;
         });
 
-        $scope.$on('$ionicView.enter', function() {
+        $scope.$on('$ionicView.enter', function () {
             $scope.isEnter = true;
         });
 
-        $scope.$on('$ionicView.leave', function() {
+        $scope.$on('$ionicView.leave', function () {
             $scope.isLeave = true;
         });
 
-        $scope.$on('$ionicView.beforeEnter', function() {
+        $scope.$on('$ionicView.beforeEnter', function () {
             $scope.isBeforeEnter = true;
         });
 
-        $scope.$on('$ionicView.beforeLeave', function() {
+        $scope.$on('$ionicView.beforeLeave', function () {
             $scope.isBeforeLeave = true;
         });
 
-        $scope.$on('$ionicView.afterEnter', function() {
+        $scope.$on('$ionicView.afterEnter', function () {
             $scope.isAfterEnter = true;
         });
 
-        $scope.$on('$ionicView.afterLeave', function() {
+        $scope.$on('$ionicView.afterLeave', function () {
             $scope.isAfterLeave = true;
         });
 
-        $scope.showAlert = function(eventType) {
+        $scope.showAlert = function (eventType) {
             $ionicPopup.alert({
                 title: 'Event Fired',
                 template: 'The event ' + eventType + ' has been fired'
@@ -165,53 +167,53 @@ angular.module('ionicDemoTabs.controllers', [])
             showDelete: false
         };
 
-        $scope.edit = function(item) {
+        $scope.edit = function (item) {
             alert('Edit Item: ' + item.id);
         };
-        $scope.share = function(item) {
+        $scope.share = function (item) {
             alert('Share Item: ' + item.id);
         };
 
-        $scope.moveItem = function(item, fromIndex, toIndex) {
+        $scope.moveItem = function (item, fromIndex, toIndex) {
             $scope.items.splice(fromIndex, 1);
             $scope.items.splice(toIndex, 0, item);
         };
 
-        $scope.onItemDelete = function(item) {
+        $scope.onItemDelete = function (item) {
             $scope.items.splice($scope.items.indexOf(item), 1);
         };
 
-        $scope.doRefresh = function() {
+        $scope.doRefresh = function () {
             var item = {id: new Date().getMilliseconds()};
             $scope.items.push(item);
-            $timeout(function() {
+            $timeout(function () {
                 // Stop the ion-refresher from spinning
                 $scope.$broadcast('scroll.refreshComplete');
             }, 1500);
         };
 
         $scope.items = [
-            { id: 0 },
-            { id: 1 },
-            { id: 2 },
-            { id: 3 },
-            { id: 4 },
-            { id: 5 },
-            { id: 6 },
-            { id: 7 },
-            { id: 8 },
-            { id: 9 },
-            { id: 10 },
-            { id: 11 },
-            { id: 12 },
-            { id: 13 },
-            { id: 14 },
-            { id: 15 },
-            { id: 16 },
-            { id: 17 },
-            { id: 18 },
-            { id: 19 },
-            { id: 20 }
+            {id: 0},
+            {id: 1},
+            {id: 2},
+            {id: 3},
+            {id: 4},
+            {id: 5},
+            {id: 6},
+            {id: 7},
+            {id: 8},
+            {id: 9},
+            {id: 10},
+            {id: 11},
+            {id: 12},
+            {id: 13},
+            {id: 14},
+            {id: 15},
+            {id: 16},
+            {id: 17},
+            {id: 18},
+            {id: 19},
+            {id: 20}
         ];
 
     });
